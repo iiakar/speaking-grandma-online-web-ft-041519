f3806e014d80a4625a6acf2ec3bf6f8f5bc43307
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-13 23:59:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,72 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(whatever)
+  
+  if whatever = (whatever).upcase 
+    return  "I LOVE YOU TOO PUMPKIN!"
+  elsif whatever != (whatever).upcase 
+return "HUH?! SPEAK UP, SONNY!"
+elsif whatever = (whatever).downcase #check
+return "'NO, NOT SINCE1938!"
+
+
+
+
+end 
+
+end
+
+
+
+
+
+#def speak_to_grandma(whatever) 
+  
+ #if whatever == "I LOVE YOU GRANDMA!" #check
+ #return "I LOVE YOU TOO PUMPKIN!"
+ 
+ #else whatever != whatever.downcase #check
+ #return 'HUH?! SPEAK UP, SONNY!'
+
+  #whatever = "WHAT DID YOU EAT TODAY?"
+  #else whatever != whatever.upcase #check
+# return "'NO, NOT SINCE1938!"
+  
+  
+  #elsif whatever = "WHAT DID YOU EAT TODAY?"
+  #return "NO, NOT SINCE 1938!"
+  
+  
+ #end 
+ 
+
+
+
+
+ 
+ 
+# else return "NO, NOT SINCE1938!" 
+ #end 
+ #end 
+ #end 
+ 
+# if whatever != whatever.upcas
+ # return 
+  
+  #else if whatever = "I L OVE YOU GRANDMA!"
+   # else puts "I LOVE YOU TOO PUMPKIN!"
+#end 
+# end 
+
+#'HUH?! SPEAK UP, SONNY!'
+#'HUH?! SPEAK UP, SONNY!'
+#"NO, NOT SINCE 1938!
+#NO, NOT SINCE 1938!"
+  
+
+
+#elsif
+# whatever != whatever.downcase 
+ #whatever = whatever.downcase 
+ #return "HUH?! SPEAK UP, SONNY!"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
